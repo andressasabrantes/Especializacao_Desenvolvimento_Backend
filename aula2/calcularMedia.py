1abb51b485b95fbe0d9637c7d702eb0a79db03bb
Andressa dos Santos Abrantes <andressabrantes@academicos.com>
2023-06-03 10:05:05 -0300

    main: Exercícios de revisão e exercícios de if/else

---
+++ aula2/calcularMedia.py
@@ -0,0 +1,8 @@
+print("Iniciando o cálculo da média: ")
+nota1 = float(input("Qual a sua nota 1?"))
+nota2 = float(input("Qual a sua nota 2?"))
+nota3 = float(input("Qual a sua nota 3?"))
+nota4 = float(input("Qual a sua nota 4?"))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print('Sua média é: ', media)
