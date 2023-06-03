84bf1eee4b69a36212c4853950ac9c12a03993c8
Andressa dos Santos Abrantes <andressabrantes@academicos.com>
2023-06-03 14:32:15 -0300

    IntroProgramacaoI: Adicionando Math.ceil() no exercício do cálculo de custo

--- aula02_Pos_IntroducaoProgramacao/calculaQntLatasTinta.py
+++ aula02_Pos_IntroducaoProgramacao/calculaQntLatasTinta.py
@@ -8,7 +8,7 @@ area_lateral = (2 * math.pi * raio * altura)
 area_base = math.pi * (raio ** 2)
 area_cilindro = area_base + area_lateral
 qtd_total_litros = area_cilindro / 3
-qtd_latas = qtd_total_litros / 5
+qtd_latas = math.ceil(qtd_total_litros / 5)
 custo = qtd_latas * 50
 
 print(f'Área Lateral: {area_lateral}')
@@ -17,5 +17,5 @@ print(f'Área Cilindro: {area_cilindro}')
 print(f'Quantidade Total de Litros: {qtd_total_litros}')
 print(f'Quantidade de Latas: {qtd_latas}')
 
-print(f'O custo total da atividade será de R${custo:.2f}.')
+print((f'O custo total da atividade será de R${custo:.2f}.'))
 
