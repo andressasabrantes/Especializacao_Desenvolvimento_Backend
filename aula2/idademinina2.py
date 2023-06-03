18898e4513838cce7109b53bef779ecf620a32bb
Andressa dos Santos Abrantes <andressabrantes@academicos.com>
2023-06-03 10:50:45 -0300

    main: if/else aninhado

--- aula2/idademinina2.py
+++ aula2/idademinina2.py
@@ -1,16 +1,20 @@
 print('-----Iniciando a verificação-----')
 
 idade = int(input('Qual a sua idade? '))
-ingresso = input('Ingresso VIP ou PISTA?')
-ingresso = ingresso.upper()
 
-if idade >= 18 and ingresso == 'VIP':
-    print('Você pode entrar na festa!')
-    print('Siga para o primeiro andar!')
-elif idade >= 18 and ingresso == 'PISTA':
-    print('Você pode entrar na festa!')
-    print('Siga pelo corredor!')
+if idade < 18:
+    print('Pode ir pra casa!')
 else:
-    print('É hora de voltar para a sua casinha!')
+    ingresso = input('Ingresso VIP ou PISTA?')
+    ingresso = ingresso.upper()
+
+    if ingresso == 'VIP':
+        print('Você pode entrar na festa!')
+        print('Siga para o primeiro andar!')
+    elif ingresso == 'PISTA':
+        print('Você pode entrar na festa!')
+        print('Siga para a pista!')
+    else:
+        print('É hora de voltar para a sua casinha!')
 
 print('-----Finalizando programa-----')
