3530cf5f9f89cf343166d1f0e4595c9121be7f72
Andressa dos Santos Abrantes <andressabrantes@academicos.com>
2023-06-03 14:18:27 -0300

    IntroducaoProgramacaoI: Exercício do custo de uma atividade

--- aula02_Pos_IntroducaoProgramacao/calculaQntLatasTinta.py
+++ aula02_Pos_IntroducaoProgramacao/calculaQntLatasTinta.py
@@ -1,2 +1,21 @@
-metros_quadrados = float(input('Quantos metros quadrados serão pintados?'))
+import math
+
+altura = float(input('Digite a altura do cilindro: '))
+raio = float(input('Digit o raio do cilindro: '))
+comprimento = (2 * math.pi * raio * altura)
+
+area_lateral = (2 * math.pi * raio * altura)
+area_base = math.pi * (raio ** 2)
+area_cilindro = area_base + area_lateral
+qtd_total_litros = area_cilindro / 3
+qtd_latas = qtd_total_litros / 5
+custo = qtd_latas * 50
+
+print(f'Área Lateral: {area_lateral}')
+print(f'Área Base: {area_base}')
+print(f'Área Cilindro: {area_cilindro}')
+print(f'Quantidade Total de Litros: {qtd_total_litros}')
+print(f'Quantidade de Latas: {qtd_latas}')
+
+print(f'O custo total da atividade será de R${custo:.2f}.')
 
