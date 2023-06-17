438a5a9a99a2bf664aee86f854f1ef3e5aeae1fb
Andressa dos Santos Abrantes <andressabrantes@academicos.com>
2023-06-17 09:19:40 -0300

    main: match case e tuplas

---
+++ aula03_Pos_IntroducaoProgramacao/aula03.2_Pos_IntroducaoProgramcao/tuplas.py
@@ -0,0 +1,15 @@
+dimensoes = (200, 50)
+
+print(dimensoes[0])
+print(dimensoes[1])
+
+#Para acessar endpoints
+endpoints = ('www.uol.com.br',
+             'www.google.com.br',
+             'www.g1.com.br')
+
+#Imprimindo endpoint
+print(endpoints[0])
+print(endpoints[1])
+print(endpoints[2])
+print(endpoints)
